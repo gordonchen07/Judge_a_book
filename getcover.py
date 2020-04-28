57f943491e4a6bf7ba9aa6f0c1aa8408207e3d22
Gordon Chen <gordon.chen07@gmail.com>
2020-04-27 20:28:23 -0400

    added if name main

--- getcover.py
+++ getcover.py
@@ -1,3 +1,13 @@
+import os
+import json
+from urllib.request import urlopen
+
+APIKEY = os.getenv("BOOKAPIKEY")
+BASE_GBOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes?q="
+NEW_DIRECTORY = 'BookCovers'
+PARENT_DIRECTORY = os.getcwd()
+path = os.path.join(PARENT_DIRECTORY, NEW_DIRECTORY)
+
 def get_cover(isbn):
     if not os.path.exists(path):
         os.makedirs(path)
@@ -7,9 +17,11 @@ def get_cover(isbn):
     try:
         image_url = book_json['items'][0]['volumeInfo']['imageLinks']['thumbnail']
         image_req = urlopen(image_url)
-        image_file = open(os.path.join(path, str(isbn_number) + '.png'), 'wb')
+        image_file = open(os.path.join(path, str(isbn) + '.png'), 'wb')
         image_file.write(image_req.read())
         image_file.close()
-    except:
+    except KeyError:
         print(str(isbn) + ' No image')
-        
+
+if __name__ == '__main__':
+    get_cover(1934876569)
